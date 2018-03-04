e943177ddebcf3eda3ccb89a0aed1a0b756c9452
rjwilliamson <37045359+rjwilliamson@users.noreply.github.com>
2018-03-04 23:45:09 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,54 @@
+function animateGoogle(name){
+    
+	const body = document.querySelector('body');
+	body.classList.add(name);
+
+	setInterval(function(){
+    
+    body.classList.remove(name);
+
+	},1000);
+	
+
+
+
+}
+
+
+function element(event,element,callback){
+	document.querySelector(element).addEventListener(event,callback);
+
+}
+
+
+element('click','#swing',function(){
+
+animateGoogle('swing');
+
+});
+
+element('click','#bounce',function(){
+
+animateGoogle('bounce');
+
+});
+
+
+element('click','#flash',function(){
+
+animateGoogle('flash');
+
+});
+
+
+element('mouseover','ul',function(){
+
+swal('STOP!!!','This site is not actually google stay away','warning');
+
+})
+
+element('click','#kill',function(){
+
+document.querySelector('body').style.display = 'none';
+
+})
